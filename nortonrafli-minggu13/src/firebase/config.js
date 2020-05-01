d4b0e41600451920960a1fa3bc2d41d64f73fafa
nortonrafli <norton.nra27@gmail.com>
2020-05-01 20:39:46 +0700

    tugas minggu 13

---
+++ nortonrafli-minggu13/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAiQDpd6Cg_1tTIDB7tpPsY0WVGknb9Ayk",
+    authDomain: "login-61135.firebaseapp.com",
+    databaseURL: "https://login-61135.firebaseio.com",
+    projectId: "login-61135",
+    storageBucket: "login-61135.appspot.com",
+    messagingSenderId: "187593101590",
+    appId: "1:187593101590:web:5e75b6c086be1dec890037",
+    measurementId: "G-69J6XCMLWD"
+  };
+
+  export default firebaseConfig; 
